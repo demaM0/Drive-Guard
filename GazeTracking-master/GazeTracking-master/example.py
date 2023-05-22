e499d151a5653ebe67a56720f3be21308cef6768
elshaymaa <shaymaamostafa50@gmail.com>
2023-05-22 17:58:15 +0300

    frames

--- GazeTracking-master/GazeTracking-master/example.py
+++ GazeTracking-master/GazeTracking-master/example.py
@@ -1,19 +1,9 @@
-"""
-Demonstration of the GazeTracking library.
-Check the README.md for complete documentation.
-"""
-
 import cv2
 from gaze_tracking import GazeTracking
+def gaze(frame,blink,right,left,center):
 
-gaze = GazeTracking()
-webcam = cv2.VideoCapture(0)
+    gaze = GazeTracking()
 
-while True:
-    # We get a new frame from the webcam
-    _, frame = webcam.read()
-
-    # We send this frame to GazeTracking to analyze it
     gaze.refresh(frame)
 
     frame = gaze.annotated_frame()
@@ -21,12 +11,16 @@ while True:
 
     if gaze.is_blinking():
         text = "Blinking"
+        blink+=1
     elif gaze.is_right():
         text = "Looking right"
+        right+=1
     elif gaze.is_left():
         text = "Looking left"
+        left+=1
     elif gaze.is_center():
         text = "Looking center"
+        center+=1
 
     cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
@@ -34,11 +28,23 @@ while True:
     right_pupil = gaze.pupil_right_coords()
     cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-
+    cv2.putText(frame, "Blink ct:  " + str(blink), (90, 200), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Right ct: " + str(right), (90, 235), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Left ct:  " + str(left), (90, 270), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Center ct: " + str(center), (90, 305), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.imshow("Demo", frame)
+    return blink,right,left,center, text
 
-    if cv2.waitKey(1) == 27:
-        break
-   
+webcam = cv2.VideoCapture(0)
+blink=0
+right=0
+left=0
+center=0
+while True:
+    _, frame = webcam.read()
+    blink,right,left,center,direction = gaze(frame,blink,right,left,center)
+    print(direction)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 webcam.release()
 cv2.destroyAllWindows()
--- objectTracking.py
+++ objectTracking.py
@@ -26,7 +26,27 @@ def parse_arguments() -> argparse.Namespace:
     return args
 
 
-def main():
+def main(frame):
+        result = model(frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_yolov8(result)
+        detections=detections[detections.class_id==0]
+        
+        frame = box_annotator.annotate(
+            scene=frame, 
+            detections=detections
+        )
+        count_text = f"People Count: {len(detections)}"
+        cv2.putText(frame, count_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        zone.trigger(detections=detections)
+        frame = zone_annotator.annotate(scene=frame)      
+        
+        cv2.imshow("yolov8", frame)
+        return len(detections)
+        
+
+
+if __name__ == "__main__":
     args = parse_arguments()
     frame_width, frame_height = args.webcam_resolution
 
@@ -51,32 +71,9 @@ def main():
         text_thickness=4,
         text_scale=2
     )
-
     while True:
         ret, frame = cap.read()
-
-        result = model(frame, agnostic_nms=True)[0]
-        detections = sv.Detections.from_yolov8(result)
-        detections=detections[detections.class_id==0]
-        # labels, = [
-        #     f"{model.model.names[class_id]} {confidence:0.2f}"
-        #     for _, confidence, class_id, _
-        #     in detections
-        # ]
-        frame = box_annotator.annotate(
-            scene=frame, 
-            detections=detections, 
-            #labels=labels
-        )
-
-        zone.trigger(detections=detections)
-        frame = zone_annotator.annotate(scene=frame)      
-        
-        cv2.imshow("yolov8", frame)
-
+        peoplecount = main(frame)
+        print(peoplecount)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-
-if __name__ == "__main__":
-    main()
